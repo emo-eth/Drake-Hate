ebb7c6f9b9c9da7eeaf13453481a31da0017c8bf
Sasha Perigo <sasha.perigo@gmail.com>
2016-04-20 02:27:41 -0700

    Expanded blacklists and moved them to a separate file.
    TODO: Get James' help with reading lists in from file! Code is clunky and I'd like to be able to have comments and blank lines in the list files.

--- twitter_bot.py
+++ twitter_bot.py
@@ -13,14 +13,15 @@ auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
 auth.set_access_token(settings.ACCESS_KEY, settings.ACCESS_SECRET)
 api = tweepy.API(auth)
 
-# Very incomplete list of music/ media blogs. Exclude tweets from 
-# these accounts, because we can assume they won't be tweeting hate!
-# TODO: Move this to a separate file or something?
-user_blacklist = ['billboard', 'pitchfork', 'ComplexMusic', 'PigsAndPlans', 'stereogum', \
-                  'ComplexMag', 'thefader', 'RapDirect', 'NoiseyMusic', 'Beats1', 'XXL', \
-                  'NME', 'MTVNews', 'SCENE', 'BleacherReport', 'SPINmagazine', 'iHeartRadio', \
-                  'boilerroomtv', 'BLUNTIQ', 'AppleMusic', 'SaintHeron', 'chartnews', 'i_D']
-word_blacklist = ['RT', u'♺']
+# Load blacklists from file
+# TODO: This is clunky af! Can I make it more Pythonic? Maybe move to helper fn?
+f = open("word_blacklist.txt")
+word_blacklist = [word.strip().lower() for word in f.readlines() if word and not word.startswith('# ')]
+f.close()
+
+f = open("user_blacklist.txt")
+user_blacklist = [user.strip() for user in f.readlines() if user != '' and user[0] != '#']
+f.close()
 
 # Store the ID of the last tweet we retweeted in a file
 # so we don't retweet things twice!
@@ -40,19 +41,22 @@ results = tweepy.Cursor(api.search, q='@Drake', since_id=savepoint, lang='en').i
 tweets = []
 for tweet in results:
     tweets.append(tweet)
-
 try:
     last_tweet_id = tweets[0].id
 except IndexError:  # No results found
     last_tweet_id = savepoint
 
+# TODO: Handle emojis better! Right now tweet.text.split() is
+# tokenizing tweets only at whitespace. It'd be nice to recognize
+# a string of emojis and process them all individually, rather 
+# than as a collective 'word'.
+
 # Filter tweets using blacklist
-tweets = [tweet for tweet in tweets if not any(word in tweet.text.split() for word in word_blacklist)]
+tweets = [tweet for tweet in tweets if not any(word.lower() in word_blacklist for word in tweet.text.split())]
 tweets = [tweet for tweet in tweets if tweet.author.screen_name not in user_blacklist]
 tweets.reverse()
 
 for i in range(len(tweets)):
-    # If "negative" (come back to this definition):
     print('(%s) %s: %s\n' % \
             (tweets[i].created_at,
              tweets[i].author.screen_name.encode('utf-8'),
