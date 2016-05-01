0c3fd518c39701bd70765fa50623dd68b169f7ad
Sasha Perigo <sasha.perigo@gmail.com>
2016-04-30 17:34:26 -0700

    Fixed blacklist shit

---
+++ user_blacklist.py
@@ -0,0 +1,29 @@
+user_blacklist = [
+
+    # Inexhaustive list of blogs
+    'billboard',
+    'pitchfork',
+    'ComplexMusic',
+    'PigsAndPlans',
+    'stereogum',
+    'ComplexMag',
+    'thefader',
+    'RapDirect',
+    'NoiseyMusic',
+    'Beats1',
+    'XXL',
+    'NME',
+    'MTVNews',
+    'SCENE',
+    'BleacherReport',
+    'SPINmagazine',
+    'iHeartRadio',
+    'boilerroomtv',
+    'BLUNTIQ',
+    'AppleMusic',
+    'SaintHeron',
+    'chartnews',
+    'i_D',
+
+    # Another Drake bot!
+    'uused2callme']
---
+++ word_blacklist.py
@@ -0,0 +1,14 @@
+word_blacklist = [
+
+  # Retweet symbols
+  'RT',
+  'u♺',
+
+  # Variants of 'listening now'
+  'u🎧',
+  '#ListeningNow',
+  '#NowListening',
+  '#NowPlaying',
+  '#NP',
+  '#OnRepeat',
+  '#Streaming']
