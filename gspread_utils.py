564e98fc11475d78f776faea41d03b64af5fe112
Sasha Perigo <sasha.perigo@gmail.com>
2016-05-29 11:24:31 -0700

    Debugging key issues on heroku

--- gspread_utils.py
+++ gspread_utils.py
@@ -26,6 +26,7 @@ def get_gspread_credentials(dev):
         private_key_id = os.environ['GSPREAD_PRIVATE_KEY_ID']
         client_id = os.environ['GSPREAD_CLIENT_ID']
 
+    print(private_key_pkcs8_pem)
     signer = crypt.Signer.from_string(private_key_pkcs8_pem)
     credentials = ServiceAccountCredentials(service_account_email, signer, scopes='https://spreadsheets.google.com/feeds', private_key_id=private_key_id, client_id=client_id)
     credentials._private_key_pkcs8_pem = private_key_pkcs8_pem
