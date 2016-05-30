8c6b7858779932ed06222e44e27c7608919d16d2
jameswenzel <wenzel.james.r@gmail.com>
2016-05-30 15:40:55 -0700

    added try block to gspread_utils.py

--- gspread_utils.py
+++ gspread_utils.py
@@ -8,10 +8,11 @@ SPREADSHEET_KEY = '1hUA03o4uOQvcywtIzlfUsP_DbDzwdShNlpsGecWSOME'
 NUM_ROWS_TO_ADD = 1000
 MAX_NUM_TWEETS = 5000
 
+
 def get_gspread_credentials(dev):
     ''' Returns a Google Spreadsheets credentials object.
     '''
-    if dev:
+    try:
         with open('gspread_settings.json', 'r') as f:
             settings = json.load(f)
 
@@ -19,24 +20,26 @@ def get_gspread_credentials(dev):
         private_key_pkcs8_pem = settings['private_key']
         private_key_id = settings['private_key_id']
         client_id = settings['client_id']
-    
-    else:
+    except:
         service_account_email = os.environ['GSPREAD_CLIENT_EMAIL']
         private_key_pkcs8_pem = os.environ['GSPREAD_PRIVATE_KEY']
         private_key_id = os.environ['GSPREAD_PRIVATE_KEY_ID']
         client_id = os.environ['GSPREAD_CLIENT_ID']
 
     signer = crypt.Signer.from_string(private_key_pkcs8_pem)
-    credentials = ServiceAccountCredentials(service_account_email, signer, scopes='https://spreadsheets.google.com/feeds', private_key_id=private_key_id, client_id=client_id)
+    credentials = ServiceAccountCredentials(service_account_email, signer,
+                                            scopes='https://spreadsheets.google.com/feeds', private_key_id=private_key_id, client_id=client_id)
     credentials._private_key_pkcs8_pem = private_key_pkcs8_pem
 
     return credentials
+
 
 def gspread_oauth(dev):
     ''' Returns a Gspread Client instance.
     '''
     credentials = get_gspread_credentials(dev)
     return gspread.authorize(credentials)
+
 
 def add_to_spreadsheet(wks, num_tweets, tweet):
     ''' Adds the text from the Tweet object to the Google Sheet.
